d31b8542176e824e8c4edd61d7be788fba1e2efe
Bryn Knatterud <bryn@actsmedia.com>
2014-12-08 16:11:06 -0600

    Fixes #1

--- app/UnfoldingWord/UnfoldingWord/LanguageList/LanguagesTableViewController.m
+++ app/UnfoldingWord/UnfoldingWord/LanguageList/LanguagesTableViewController.m
@@ -35,7 +35,7 @@
     
     [self registerForNotifications];
     
-    self.navigationItem.title = @"Unfolding Word";
+    self.navigationItem.title = @"unfoldingWord";
     [[UIApplication sharedApplication] setStatusBarStyle:UIStatusBarStyleLightContent];
     
     self.navigationController.navigationBar.titleTextAttributes = [NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor], NSForegroundColorAttributeName, nil];
